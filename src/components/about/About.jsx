c9cf8f2b089ca9abff9bdebe30ff75e12e65c66b
Steven Huang <22431150+EvenStevenH@users.noreply.github.com>
2026-07-16 15:21:01 -0400

    add table

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -25,22 +25,38 @@ export default function About() {
 						>
 							Steven Huang
 						</a>
-						, that helps creatives discover events! It utilizes frontend technologies such as HTML for structure, CSS for styling, JavaScript for interactivity, and React for component-based UI and state management.
+						, that helps creatives discover events!
 					</p>
 
 					<ul>
-						<li>A few features of this demo app include:</li>
-						<ul>
-							<li>An event page to view and save events, with event details in a separate view</li>
-							<li>A dashboard with a mock social media feed, your saved events, and a simple tool to roll random drawing subjects and color harmonies</li>
-							<li>A profile page to view information about you and your friends</li>
-						</ul>
+						<li>Utilizes frontend technologies such as HTML for structure, CSS for styling, JavaScript for interactivity, and React for component-based UI and state management.</li>
 
-						<li>Trivia on the naming:</li>
-						<ul>
-							<li>A "hearth" is the area around a fireplace (and sounds like "heart"). A "hue" is a color or pigment, standing for arts and creativity (and also sounds like "you"). Together, the name inspires a cozy gathering place where we can connect our community members!</li>
-						</ul>
+						<li>Naming trivia! A "hearth" is the area around a fireplace (and sounds like "heart"). A "hue" is a color or pigment, standing for arts and creativity (and also sounds like "you"). Together, the name inspires a cozy gathering place where we can connect our community!</li>
 					</ul>
+
+					<table className="aboutTable">
+						<thead>
+							<tr>
+								<th id="aboutTableLeft">Feature</th>
+								<th id="aboutTableRight">Description</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>Dashboard</td>
+								<td>A mock social media feed, your saved events, and a simple tool to roll random subjects and color harmonies.</td>
+							</tr>
+							<tr>
+								<td>Events</td>
+								<td>View and save events, with event details in a separate view.</td>
+							</tr>
+
+							<tr>
+								<td>Your Profile</td>
+								<td>View information about you and your friends.</td>
+							</tr>
+						</tbody>
+					</table>
 				</div>
 			</section>
 
